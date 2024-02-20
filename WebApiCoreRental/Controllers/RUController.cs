c254a5a46e6428229adf82251077ce2aec904f36
Valerie Roussel <valr30@outlook.com>
2024-02-20 12:29:59 -0500

    Add project files.

---
+++ WebApiCoreRental/Controllers/RUController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+
+namespace WebApiCoreRental.Controllers
+{
+    public class RUController : Controller
+    {
+        // requires using Microsoft.Extensions.Configuration;
+        private readonly IConfiguration _configuration;
+
+        public RUController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost(Name = "AddRental")]
+        // POST: api/RM
+        //   [ResponseType(typeof(string))]
+        public string AddRental( RentalUnit.Rental aRent)
+        {
+            int rows = -1;
+            string trace = "";
+            try
+            {
+
+                string connStr = _configuration.GetSection("ConnectionStrings").GetSection("RegionMonitor").Value;
+
+
+                MySqlConnection conn = new MySqlConnection(connStr);
+                MySqlCommand cmd = new MySqlCommand("AddRental", conn);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@RegionName", aRent.RegionName);
+                cmd.Parameters.AddWithValue("@RentName", aRent.RentName);
+                cmd.Parameters.AddWithValue("@RentalTimeStamp", aRent.RentalTimeStamp);
+                cmd.Parameters.AddWithValue("@RentalDateExpirency", aRent.RentalDateExpirency);
+                cmd.Parameters.AddWithValue("@RenterName", aRent.RenterName);
+                cmd.Parameters.AddWithValue("@IdRenter", aRent.IdRenter);
+                cmd.Parameters.AddWithValue("@RenterGender", aRent.RenterGender);
+                cmd.Parameters.AddWithValue("@RentalAction", aRent.RentalAction);
+                cmd.Parameters.AddWithValue("@NbDays", aRent.NbDays);
+                cmd.Parameters.AddWithValue("@Amount", aRent.Amount);
+                trace += "\n4";
+                conn.Open();
+
+                rows = cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                return string.Format("Exception = " + ex.Message + " \n" + trace + "\n" + ex.ToString(), rows);
+
+            }
+
+            return rows.ToString();
+        }
+    }
+}
---
+++ WebApiCoreRental/RentalUnit.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApiCoreRental
+{
+    public class RentalUnit
+    {
+        public class Rental
+        {
+            public string RegionName { get; set; } = "";
+            public string RentName { get; set; } = "";
+            public DateTime RentalTimeStamp { get; set; }
+            public DateTime RentalDateExpirency { get; set; }
+            public string RenterName { get; set; } = "";
+            public string IdRenter { get; set; } = "";
+            public string RenterGender { get; set; } = "";
+            public string RentalAction { get; set; } = "";
+            public string NbDays { get; set; } = "";
+            public string Amount { get; set; } = "";
+
+        }
+    }
+}
