69b8b0d3117f821c4af59750735131e16edb61e3
Valerie Roussel <110930756+valr300@users.noreply.github.com>
2024-02-24 14:10:17 -0500

    Update Rental_Rentals.sql

--- Rental_Rentals.sql
+++ Rental_Rentals.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `Rental` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `Rental`;
 -- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
 --
 -- Host: localhost    Database: Rental
@@ -56,4 +58,4 @@ CREATE TABLE `Rentals` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-02-23 12:13:05
+-- Dump completed on 2024-02-24 14:08:40
