6b3b37ae4b2f3c6636265200c2bb9b2d7c4b8dfe
Valerie Roussel <110930756+valr300@users.noreply.github.com>
2024-05-19 09:07:39 -0400

    Add files via upload
    For initial install 2.01

--- Rental_Rentals.sql
+++ Rental_Rentals.sql
@@ -1,3 +1,8 @@
+CREATE DATABASE  IF NOT EXISTS `Rental` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `Rental`;
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
+--
+
 -- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
 --
 -- Host: localhost    Database: Rental
